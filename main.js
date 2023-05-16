dce670e2ab7a6ee3521f236c5fd12c941f93e355
quartz <quartz@quartzs-MacBook-Pro.local>
2023-05-17 04:49:13 +0900

    本日の進捗

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
-let leftButton = document.getElementById("left");
-let rightButton = document.getElementById("right");
+let backButton = document.getElementById("back");
+let nextButton = document.getElementById("next");
+let item = document.getElementsByClassName("p-index-fv__item");
 let plus = 0;
+let itemWidth = 0;
 
-leftButton.addEventListener("click", function () {
-  let img = document.getElementsByClassName("p-index-fv__item");
-  console.log(img);
-  plus += 1000;
-  for (let i = 0; i < img.length; i++) {
-    img[i].style.transform = `translateX(-${plus}px)`;
+
+// window.addEventListener('resize' , function(){
+//   let itemWidth = item[0].clientWidth;
+//   console.log(itemWidth);
+// })
+
+const imgResize = function(){
+     let itemWidth = item[0].clientWidth;
+     console.log(itemWidth);
+     console.log("imgResizeが発動しました");
+   }
+
+// imgResize();
+window.addEventListener('resize', imgResize); //←これが動かない理由がわからない
+imgResize();
+
+// clickイベント内にresizeイベント内の変数を使用したい
+// console.log(window.addEventListener('resize', imgResize)());
+
+// 戻るボタン
+backButton.addEventListener('click', function(){
+  if(plus > 0){
+    plus -= itemWidth;
+    console.log("減ってます");
+  }else{
+    console.log("もう減らない：0です");
   }
-  console.log("左が押されました！");
-  console.log(img[0]);
-});
-rightButton.addEventListener("click", function () {
-  let img = document.getElementsByClassName("p-index-fv__item");
-  plus += 1000;
-  for (let i = 0; i < img.length; i++) {
-    img[i].style.transform = `translateX(${plus}px)`;
+  for (let i = 0; i < item.length; i++) {
+    item[i].style.transform = `translateX(-${plus}px)`;
   }
-  console.log("右が押されました！");
-  console.log(img[0]);
+  console.log("戻る");  
 });
-// rightButton.addEventListener("click", function () {
-//   console.log("右が押されました！");
-// });
+
+
+// 次へボタン
+nextButton.addEventListener('click', function(){
+  // clickした時、plusがスライドの最大値(itemの長さから1引いた数にitemの横幅をかけた)より小さい場合itemの横幅を加算していく
+  if(plus < (item.length - 1) * itemWidth){
+    plus += itemWidth;
+    console.log("増えてます");
+  }else{
+    console.log("もう増えないMAXです");
+  }
+  for (let i = 0; i < item.length; i++) {
+    item[i].style.transform = `translateX(-${plus}px)`;
+  }
+  console.log(plus);
+  console.log("すすむ");
+})
